800eed3fd58ed7427e5c728277fec88c004caad7
Milo <milolmt@gmail.com>
2015-11-25 20:50:25 +0100

    Create simulation.h

---
+++ simulation.h
@@ -0,0 +1,22 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+#include <vector>
+#include <list>
+#include <utility>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class simulation
+{
+    int acc(const vector<pair<int,int>>&);
+    int acc2(const vector<pair<int,int>>&);
+    void reduce(vector<pair<int,int>>&,vector<pair<int,int>>&);
+public:
+    simulation();
+    int my_simulation(vector<int>&);
+};
+
+#endif // SIMULATION_H
